75dd53acec5227ef7a7742d1d652a3d1e64fca24
James West <james.west@westdarley.co.uk>
2016-10-09 20:10:20 +0100

    updated with proper args in zbtprinter.js

--- www/zbtprinter.js
+++ www/zbtprinter.js
@@ -1,13 +1,10 @@
 var exec = require('cordova/exec');
 
-exports.print = function(str, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'print', [str]);
+exports.print = function(str, mac, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'print', [str, mac]);
 };
 
 exports.getprinter = function(successCallback, errorCallback){
     cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'getprinter', []);
 };
 
-exports.getprinter = function(successCallback, errorCallback){
-    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'getprinter', []);
-};
